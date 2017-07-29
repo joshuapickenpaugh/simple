5e8196e0c965132bf86a3a7ad408d09106c3e3be
joshuapickenpaugh <joshuapickenpaugh@hotmail.com>
2017-07-29 16:26:44 -0500

    Final sync.

--- App/Program.cs
+++ App/Program.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿//Joshua Pickenpaugh
+//July 29, 2017
+//Simple
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +14,25 @@ namespace App
     {
         static void Main(string[] args)
         {
+            List<double> lstDouble = new List<double>();
+
+            Console.WriteLine("How many numbers do you wish to enter?:");
+            int intUserSelect = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter some numbers: ");
+            for (int x = 1; x <= intUserSelect; x++)
+            {
+                double dblUserNum = Convert.ToDouble(Console.ReadLine());
+                lstDouble.Add(dblUserNum);
+            }
+
+            foreach (double num in lstDouble)
+            {
+                Console.Write(num + ",");
+            }
+
+            Console.ReadKey();
+
         }
     }
 }
